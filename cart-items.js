e80e5fc8ca58f79129549971c6af0b8af91c4fa9
Lenny Hughes <lenaudhughes@gmail.com>
2019-08-26 18:19:43 -0400

    Adding database and changing API routes

--- cart-items.js
+++ cart-items.js
@@ -1,65 +1,83 @@
 const express = require('express');
+const pool = require('./pg-connection-pool');
+
 const cartItemRtr = express.Router();
-const bodyParser = require('body-parser');
 
-const cartItems = [
-    {
-        id:"1",
-        product:"Natural Deodorant",
-        price:"10.99",
-        quantity: "20"
-    },
-    {
-        id:"2",
-        product:"Almond Milk Shampoo",
-        price:"17.99",
-        quantity: "5"
-    },
-    {
-        id:"3",
-        product:"Organic Toothpaste",
-        price:"11.99",
-        quantity: "8"
-    },
-    {
-        id:"4",
-        product:"Earth Face Scrub",
-        price:"19.99",
-        quantity: "25"
-    }
-];
-
-cartItemRtr.use(bodyParser.json());
 
 cartItemRtr
 
     .get("/cart-items", (req,res)=>{
         res.setHeader("Content-Type", "application/json");
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(cartItems);
-        res.end();
+
+        const query = "SELECT id,product,price, quantity FROM ShoppingCart";
+
+        pool.query(query).then((response) => {
+
+            if(response.rows.length > 0){
+                res.status(200).json(response.rows);
+            }
+            else {
+                res.status(400).send();
+            }
+        }).catch(err => {
+            console.log(err);
+            res.status(400).send();
+        })
     })
     .post("/cart-items", (req,res)=>{
+        const postBody = req.body;
+
         res.setHeader("Content-Type", "application/json");
         res.setHeader('Access-Control-Allow-Origin', '*');
-        console.log(req.body);
-        res.send("post");
-        res.end();
+
+        const query = "INSERT INTO ShoppingCart (product,price, quantity) VALUES ($1, $2, $3)";
+
+        pool.query(query, [postBody.product, postBody.price, postBody.quantity]).then(() => {
+            res.status(201).json({
+                product: postBody.product,
+                price: postBody.price,
+                quantity: postBody.quantity
+            });
+        }).catch( err => {
+            console.log(err);
+            res.status(500).send();
+        })
     })
-    .put("/cart-items/:id", (req,res)=>{
+    .put("/cart-items/:product", (req,res)=>{
+
+        const product = req.params.product;
+        const putBody = req.body;
+
         res.setHeader("Content-Type", "application/json");
         res.setHeader('Access-Control-Allow-Origin', '*');
-        console.log(req.body);
-        console.log(req.params.id);
-        res.send("put");
-        res.end();
+
+        const query = "UPDATE ShoppingCart set quantity=$1 WHERE product=$2";
+        pool.query(query, [putBody.quantity, product]).then(() => {
+            res.status(200).json({
+                product:product,
+                quantity:putBody.quantity
+            });
+        }).catch( err => {
+            console.log(err);
+            res.status(500).send();
+        })
+        
     })
     .delete("/cart-items/:id", (req,res)=>{
+        const productId = req.params.id;
+        
         res.setHeader("Content-Type", "application/json");
         res.setHeader('Access-Control-Allow-Origin', '*');
-        console.log(req.params.id);
-        res.send("delete");
-        res.end();
+        
+        const query = "DELETE from ShoppingCart WHERE ID = $1";
+
+        pool.query(query, [productId]).then(() => {
+            res.status(200).json('item removed');
+        }).catch( err => {
+            console.log(err);
+            res.status(500).send();
+        })
     })
 
     module.exports = cartItemRtr;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const port = 8888;
 const express = require("express");
+const cors = require('cors');
 const cartItemRoutes = require('./cart-items');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(cartItemRoutes);
+app
+    .use(cors())
+    .use(bodyParser.json())
+    .use(cartItemRoutes)
 
 app.listen(port, () => {
         console.log(`Server Running at port:${port}`);
